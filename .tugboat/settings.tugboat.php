96c3947ffb31d028c3f35f2e1b037746ef7b0404
KeyboardCowboy <chris.albrecht@lullabot.com>
2023-07-31 18:56:25 -0600

    Update config for new database

--- .tugboat/settings.tugboat.php
+++ .tugboat/settings.tugboat.php
@@ -15,7 +15,7 @@ $databases['default']['default'] = array (
 // Use the TUGBOAT_REPO_ID to generate a hash salt for Tugboat sites.
 $settings['hash_salt'] = hash('sha256', getenv('TUGBOAT_REPO_ID'));
 
-$settings['config_sync_directory'] = getenv('TUGBOAT_ROOT') . '/config';
+$settings['config_sync_directory'] = getenv('TUGBOAT_ROOT') . '/config/sync';
 
 $settings['update_free_access'] = FALSE;
 
